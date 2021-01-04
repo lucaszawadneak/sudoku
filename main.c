8ea92f0fe35c8bee34b32d35b4d38f8c57af42de
Lucas <lucaszawadneak@gmail.com>
2021-01-04 15:55:49 -0300

    Missing in line & column

--- main.c
+++ main.c
@@ -6,31 +6,32 @@
 #include <stdio.h>
 
 #define TAMANHO 9
+#define POSSIVEIS [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
 
-void inicializaVetor(int v[], int tam)
-{
-    int i;
-    for (i = 0; i < tam; i++)
-    {
-        v[i] = 0;
-    }
-}
+// void inicializaVetor(int v[], int tam)
+// {
+//     int i;
+//     for (i = 0; i < tam; i++)
+//     {
+//         v[i] = 0;
+//     }
+// }
 
-void inicializa3Vetor(int v1[], int v2[], int v3[], int tam)
-{
-    inicializaVetor(v1, tam);
-    inicializaVetor(v2, tam);
-    inicializaVetor(v3, tam);
-}
+// void inicializa3Vetor(int v1[], int v2[], int v3[], int tam)
+// {
+//     inicializaVetor(v1, tam);
+//     inicializaVetor(v2, tam);
+//     inicializaVetor(v3, tam);
+// }
 
 void showSudoku(int mat[][TAMANHO])
 {
     int i, j;
     printf("\n +-----------+-----------+-----------+\n");
-    for (i = 0; i < 9; i++)
+    for (i = 0; i < TAMANHO; i++)
     {
         printf(" | ");
-        for (j = 0; j < 9; j++)
+        for (j = 0; j < TAMANHO; j++)
         {
             if (mat[i][j] > 0)
             {
@@ -54,8 +55,84 @@ void showSudoku(int mat[][TAMANHO])
     }
 }
 
-void solveGame(int mat[][TAMANHO])
+void findMissingColumn(int mat[][TAMANHO], int pos)
 {
+    int i, j, k;
+    int invalidos[9];
+
+    for (j = 1; j <= TAMANHO; j++)
+    {
+        for (i = 0; i < TAMANHO; i++)
+        {
+            if (j == mat[i][pos])
+            {
+                for (k = 0; 0 == 0; k++)
+                {
+                    if (invalidos[k] == '\0')
+                    {
+                        invalidos[k + 1] = '\0';
+                        invalidos[k] = mat[i][pos];
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
+
+void findMissingLine(int mat[][TAMANHO], int pos)
+{
+    int i, j, k;
+    int invalidos[9];
+
+    for (j = 1; j <= TAMANHO; j++)
+    {
+        for (i = 0; i < TAMANHO; i++)
+        {
+            if (j == mat[pos][i])
+            {
+                for (k = 0; 0 == 0; k++)
+                {
+                    if (invalidos[k] == '\0')
+                    {
+                        invalidos[k + 1] = '\0';
+                        invalidos[k] = mat[pos][i];
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
+
+void findMissingGroup()
+{
+}
+
+int findMatch(int mat[][TAMANHO], int line, int col, int groupLine, int groupCol)
+{
+    int lineMatches, colMatches, groupMatches;
+
+    return 0;
+}
+
+int solveGame(int mat[][TAMANHO])
+{
+    int line, col, groupLine, groupCol, value;
+    // [LINHA,COLUNA];
+    // GROUP = QUADRADOS MENORES DO SUDOKU (DE 1 a 9)
+    for (line = 1; line <= TAMANHO; line++)
+    {
+        groupLine = (line - 1) / 3;
+        for (col = 1; col <= TAMANHO; col++)
+        {
+            // printf("%i\n", mat[line][col]);
+            // groupCol = (col - 1) / 3;
+            // mat[line][col] = findMatch(mat, line, col, groupLine, groupCol);
+        }
+    }
 
     showSudoku(mat);
 }
@@ -79,6 +156,8 @@ int main()
 
     printf("Configuracao inicial do jogo:\n");
     showSudoku(solucao);
+
+    findMissingColumn(solucao, 0);
 
     printf("%s", "\nSolução:\n");
     solveGame(solucao);
